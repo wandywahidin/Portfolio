19894f13f350bdbb5a1508a953f78b8272f7997e
wandy <wandywahidin53@gmail.com>
2022-08-11 12:10:09 +0700

    fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <div className=" max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-[#00ff00]">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Wandy Wahidin tes
+          Wandy Wahidin
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Front-End Developer
